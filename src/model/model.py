dc6c7ac3125a868d8bc20ad017d1fc136646c26e
SephirZ <62177243+SephirZ@users.noreply.github.com>
2022-01-27 20:07:43 +0100

    Update model.py

--- src/model/model.py
+++ src/model/model.py
@@ -1,31 +1,40 @@
 import pandas as pds
 df = pds.read_csv("mergedTab.csv", encoding='ISO-8859-1', sep=';')
 top=[]
-df = df.apply (pds.to_numeric, errors='coerce')
-df = df.dropna()
-print(df)
+df['views'] = df['views'].fillna(0)
+df = df.drop_duplicates(subset=['video_id'], keep='last')
 for i in range(len(df)):
     top.append(df.iloc[i,7])
-print(top)
-#for i in range(len(top)):
-
-#    if str(top[i])=='nan':
-#        top[i]='0'
-#top = map(int, top)
-#top = list(map(int, top))
+for i in range(len(top)):
+    if str(top[i])=='nan':
+       top[i]='0'
+top = map(int, top)
+top = list(map(int, top))
 top.sort(reverse=True)
 T1=top[0]
 T2=top[1]
 T3=top[2]
 T4=top[3]
 T5=top[4]
-print(str(T5))
-print(str(round(df.iloc[38218,7])))
-
+for i in range(len(df)):
+    if round(df.iloc[i,7])==(T5):
+        print('TOP 5 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
 
 for i in range(len(df)):
-    if str(round(df.iloc[i,7]))==str(T5):
-        print(df.iloc[i,0]) 
+    if round(df.iloc[i,7])==(T4):
+        print('TOP 4 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,7])==(T3):
+        print('TOP 3 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,7])==(T2):
+        print('TOP 2 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,7])==(T1):
+        print('TOP 1 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
 
 
 def ComputeMean():
