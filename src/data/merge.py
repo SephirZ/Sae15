f8d50e75163ddc75ebd4f8f6586a140b1af95dd4
SephirZ <62177243+SephirZ@users.noreply.github.com>
2022-01-28 08:54:25 +0100

    Update merge.py

--- src/data/merge.py
+++ src/data/merge.py
@@ -1,15 +1,15 @@
-# importing libraries
+# importing des modules
 import pandas as pd
 import glob
 import os
 
-# merging the files
+# detection des ficchiers
 joined_files = os.path.join('U:\Documents\TESTCSV', "youtube*.csv")
 
-# A list of all joined files is returned
+# une liste des fichiers est créer.
 joined_list = glob.glob(joined_files)
 
-# Finally, the files are joined
+# Les fichiers sont fusionnés
 df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
 print(df)
 export_csv = df.to_csv ('Donnees.csv', index=None, header=True, encoding='utf-8', sep=',')
