7658462f244342909eb86234bb33eb2aedf3276e
SephirZ <62177243+SephirZ@users.noreply.github.com>
2022-01-28 10:48:20 +0100

    Update model.py

--- src/model/model.py
+++ src/model/model.py
@@ -1,5 +1,5 @@
 import pandas as pds
-df = pds.read_csv("mergedTab.csv", encoding='ISO-8859-1', sep=';')
+df = pds.read_csv("../../data/processed/mergedTab.csv", encoding='ISO-8859-1', sep=';')
 top=[]
 df['views'] = df['views'].fillna(0)
 df = df.drop_duplicates(subset=['video_id'], keep='last')
@@ -16,6 +16,7 @@ T2=top[1]
 T3=top[2]
 T4=top[3]
 T5=top[4]
+print('TOP 5 DES VIDEOS LES PLUS VUES')
 for i in range(len(df)):
     if round(df.iloc[i,7])==(T5):
         print('TOP 5 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
@@ -35,6 +36,49 @@ for i in range(len(df)):
 for i in range(len(df)):
     if round(df.iloc[i,7])==(T1):
         print('TOP 1 : {} avec un total de {} vues ! '.format(df.iloc[i,2],round(df.iloc[i,7])))
+print()
+print('TOP 5 DES VIDEOS AVEC LE PLUS DE LIKES')
+
+top2=[]
+df['likes'] = df['likes'].fillna(0)
+df = df.drop_duplicates(subset=['video_id'], keep='last')
+for i in range(len(df)):
+    top2.append(df.iloc[i,8])
+for i in range(len(top2)):
+    if str(top2[i])=='nan':
+       top2[i]='0'
+top2 = map(int, top2)
+top2 = list(map(int, top2))
+top2.sort(reverse=True)
+T11=top2[0]
+T22=top2[1]
+T33=top2[2]
+T44=top2[3]
+T55=top2[4]
+for i in range(len(df)):
+    if round(df.iloc[i,8])==(T55):
+        print('TOP 5 : {} avec un total de {} likes ! '.format(df.iloc[i,2],round(df.iloc[i,8])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,8])==(T44):
+        print('TOP 4 : {} avec un total de {} likes ! '.format(df.iloc[i,2],round(df.iloc[i,8])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,8])==(T33):
+        print('TOP 3 : {} avec un total de {} likes ! '.format(df.iloc[i,2],round(df.iloc[i,8])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,8])==(T22):
+        print('TOP 2 : {} avec un total de {} likes ! '.format(df.iloc[i,2],round(df.iloc[i,8])))
+
+for i in range(len(df)):
+    if round(df.iloc[i,8])==(T11):
+        print('TOP 1 : {} avec un total de {} likes ! '.format(df.iloc[i,2],round(df.iloc[i,8])))
+print()
+print('Moyenne des likes et dislikes : ')
+
+
+
 
 def ComputeMean(df):
     mean = 0
